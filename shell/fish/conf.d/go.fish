ec9222149fa4155796237e2b643fd31316ebe980
Excute <oys1751@gmail.com>
2024-11-12 15:23:10 +0900

    add go1.23.3 path

--- shell/fish/conf.d/go.fish
+++ shell/fish/conf.d/go.fish
@@ -1,5 +1,6 @@
 export GO_LOCAL_BIN=/usr/local/go/bin
 export GO_1_22_8_BIN=/home/oys/sdk/go1.22.8/bin
 export GO_1_23_2_BIN=/home/oys/sdk/go1.23.2/bin
+export GO_1_23_3_BIN=/home/oys/sdk/go1.23.3/bin
 
 fish_add_path /home/oys/go/bin
