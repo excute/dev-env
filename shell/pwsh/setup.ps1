201904907ee9d0d412165521cf0e358a57429c74
excute <oys1751@gmail.com>
2024-12-11 11:27:20 +0900

    pwsh revert posh setup script

--- shell/pwsh/setup.ps1
+++ shell/pwsh/setup.ps1
@@ -3,8 +3,8 @@ if (Test-Path "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.p
 }
 Copy-Item "./Microsoft.PowerShell_profile.ps1" -Destination "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
 
-# if (Test-Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml") {
-# 	Remove-Item "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml"
-# }
-# Copy-Item "../oh-my-posh/oh-my-posh.yaml" -Destination "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml"
+if (Test-Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml") {
+	Remove-Item "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml"
+}
+Copy-Item "../oh-my-posh/oh-my-posh.yaml" -Destination "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml"
 
