578eefda78be05d80bbff489c4202e4569948f7c
excute <oys1751@gmail.com>
2025-01-20 12:46:21 +0900

    fix windows ssh linking

--- ssh/windows-link-authorized-keys.ps1
+++ ssh/windows-link-authorized-keys.ps1
@@ -1,5 +1,10 @@
+$sourcePath = "./authorized_keys"
+$targetPath = "C:\ProgramData\ssh\authorized_keys"
+
 if (Test-Path "$env:USERPROFILE/.ssh/authorized_keys") {
 	Remove-Item "$env:USERPROFILE/.ssh/authorized_keys"
 }
 
-New-Item -Path "$env:USERPROFILE/.ssh/authorized_keys" -Value ./authorized_keys -ItemType SymbolicLink
+# New-Item -Path "$env:USERPROFILE/.ssh/authorized_keys" -Value ./authorized_keys -ItemType SymbolicLink
+
+New-Item -Path $targetPath -Value $sourcePath -ItemType SymbolicLink
