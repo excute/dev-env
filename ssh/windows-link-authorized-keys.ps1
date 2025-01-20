094feba83aa6f0a9e5b4f5fb6e55eb699a2d7e42
excute <oys1751@gmail.com>
2025-01-20 12:54:19 +0900

    fix windows ssh authorized key file with hard link

--- ssh/windows-link-authorized-keys.ps1
+++ ssh/windows-link-authorized-keys.ps1
@@ -5,6 +5,4 @@ if (Test-Path $targetPath) {
 	Remove-Item $targetPath
 }
 
-# New-Item -Path "$env:USERPROFILE/.ssh/authorized_keys" -Value ./authorized_keys -ItemType SymbolicLink
-
-New-Item -Path $targetPath -Value $sourcePath -ItemType SymbolicLink
+New-Item -Path $targetPath -Value $sourcePath -ItemType HardLink
