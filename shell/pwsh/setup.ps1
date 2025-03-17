70064fd80ae6b6d35bae265536696dca67c86196
Excute <oys1751@gmail.com>
2025-03-17 14:05:25 +0900

    git config for pwsh

--- shell/pwsh/setup.ps1
+++ shell/pwsh/setup.ps1
@@ -1,10 +1,19 @@
+# Shell
+## Powershell config
 if (Test-Path "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1") {
 	Remove-Item "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
 }
 Copy-Item "./Microsoft.PowerShell_profile.ps1" -Destination "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
 
+## Oh-my-posh
 if (Test-Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml") {
 	Remove-Item "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml"
 }
 Copy-Item "../oh-my-posh/oh-my-posh.yaml" -Destination "$env:OneDrive/Documents/PowerShell/oh-my-posh.yaml"
 
+
+# git
+if (Test-Path "$env:USERPROFILE/.gitconfig") {
+	Remove-Item "$env:USERPROFILE/.gitconfig"
+}
+New-Item -Path "$env:USERPROFILE/.gitconfig" -ItemType HardLink -Value "../../git/.gitconfig"
