2ba47382e5bf5872b64dcd573872c1c73bde6693
Excute <oys1751@gmail.com>
2025-04-14 14:05:26 +0900

    add go version 1.23.8, 1.24.2

--- shell/fish/conf.d/go.fish
+++ shell/fish/conf.d/go.fish
@@ -5,5 +5,7 @@ export GO_1_22_8_BIN=/home/oys/sdk/go1.22.8/bin
 export GO_1_23_2_BIN=/home/oys/sdk/go1.23.2/bin
 export GO_1_23_3_BIN=/home/oys/sdk/go1.23.3/bin
 export GO_1_23_4_BIN=/home/oys/sdk/go1.23.4/bin
+export GO_1_23_8_BIN=/home/oys/sdk/go1.23.8/bin
+export GO_1_24_2_BIN=/home/oys/sdk/go1.24.2/bin
 
 fish_add_path /home/oys/go/bin
