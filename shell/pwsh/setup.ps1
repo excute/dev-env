5acf52ec3670635ac484b6c1de5010878d54c4a0
Excute <oys1751@gmail.com>
2025-05-12 17:24:20 +0900

    improve pwsh setup script

--- shell/pwsh/setup.ps1
+++ shell/pwsh/setup.ps1
@@ -3,22 +3,26 @@
 if (Test-Path "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1") {
 	Remove-Item "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
 }
-Copy-Item "./Microsoft.PowerShell_profile.ps1" -Destination "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
+# Copy-Item "./Microsoft.PowerShell_profile.ps1" -Destination "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
+New-Item -Path "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1" -ItemType HardLink -Value "./Microsoft.PowerShell_profile.ps1"
+# New-Item -Path "$env:OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1" -ItemType SymbolicLink -Target "./Microsoft.PowerShell_profile.ps1"
 
 ## Oh-my-posh
 if (Test-Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.json") {
 	Remove-Item "$env:OneDrive/Documents/PowerShell/oh-my-posh.json"
 }
-New-Item -Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.json" -ItemType HardLink -Value "../oh-my-posh/oh-my-posh.json"
 # Copy-Item "../oh-my-posh/oh-my-posh.json" -Destination "$env:OneDrive/Documents/PowerShell/oh-my-posh.json"
+New-Item -Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.json" -ItemType HardLink -Value "../oh-my-posh/oh-my-posh.json"
+# New-Item -Path "$env:OneDrive/Documents/PowerShell/oh-my-posh.json" -ItemType SymbolicLink -Target "../oh-my-posh/oh-my-posh.json"
 
 
 # git
 if (Test-Path "$env:USERPROFILE/.gitconfig") {
 	Remove-Item "$env:USERPROFILE/.gitconfig"
 }
-New-Item -Path "$env:USERPROFILE/.gitconfig" -ItemType HardLink -Value "../../git/.gitconfig"
 # Copy-Item "../../git/.gitconfig" -Destination "$env:USERPROFILE/.gitconfig"
+New-Item -Path "$env:USERPROFILE/.gitconfig" -ItemType HardLink -Value "../../git/.gitconfig"
+# New-Item -Path "$env:USERPROFILE/.gitconfig" -ItemType SymbolicLink -Target "../../git/.gitconfig"
 
 
 # go
@@ -27,3 +31,4 @@ if (Test-Path "$env:USERPROFILE/.golangci.yaml") {
 	Remove-Item "$env:USERPROFILE/.golangci.yaml"
 }
 New-Item -Path "$env:USERPROFILE/.golangci.yaml" -ItemType HardLink -Value "../../go/.golangci.yaml"
+# New-Item -Path "$env:USERPROFILE/.golangci.yaml" -ItemType SymbolicLink -Target "../../go/.golangci.yaml"
