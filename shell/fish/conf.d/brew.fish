18e5ffaa8a4dc6b15c841f5430679fb53f417412
Excute <oys1751@gmail.com>
2025-05-28 14:34:48 +0900

    fix fish brew init script

--- shell/fish/conf.d/brew.fish
+++ shell/fish/conf.d/brew.fish
@@ -1,1 +1,9 @@
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
+
+if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+if not contains "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; set --global --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; end;
